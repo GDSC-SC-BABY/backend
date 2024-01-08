769e4bf1b516b31363460c09e8ee623636686e5e
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-01-08 17:24:40 +0900

    setting

---
+++ src/main/java/com/example/scbaby/ScBabyApplication.java
@@ -0,0 +1,13 @@
+package com.example.scbaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScBabyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScBabyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scbaby/ScBabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scbaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScBabyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
