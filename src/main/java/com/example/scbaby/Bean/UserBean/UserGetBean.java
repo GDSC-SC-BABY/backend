a49bfccb8ab1a930fb4f60ddfa76ff34c13af25c
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-01-08 20:03:47 +0900

    Add User API

---
+++ src/main/java/com/example/scbaby/Bean/UserBean/UserGetBean.java
@@ -0,0 +1,17 @@
+package com.example.scbaby.Bean.UserBean;
+
+import com.example.scbaby.Model.DTO.StateRes;
+import com.example.scbaby.Model.DTO.User.Res.UserGetRes;
+import com.example.scbaby.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserGetBean {
+    private final UserRepository userRepository;
+
+    public UserGetRes exec(String userId) {
+        return UserGetRes.of(userRepository.findById(userId).get());
+    }
+}
---
+++ src/main/java/com/example/scbaby/Bean/UserBean/UserRegistBean.java
@@ -0,0 +1,18 @@
+package com.example.scbaby.Bean.UserBean;
+
+import com.example.scbaby.Model.DTO.StateRes;
+import com.example.scbaby.Model.DTO.User.Req.UserRegistReq;
+import com.example.scbaby.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserRegistBean {
+    private final UserRepository userRepository;
+
+    public StateRes exec(UserRegistReq userRegistReq) {
+        userRepository.save(userRegistReq.toDAO());
+        return new StateRes(true);
+    }
+}
