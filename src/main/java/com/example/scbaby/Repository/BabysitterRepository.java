408d8dce9d88bb1ba6e6b599666b007aff2897b5
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-01-08 20:02:55 +0900

    Add Repository

---
+++ src/main/java/com/example/scbaby/Repository/BabysitterRepository.java
@@ -0,0 +1,9 @@
+package com.example.scbaby.Repository;
+
+import com.example.scbaby.Model.DAO.BabysitterDAO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BabysitterRepository extends JpaRepository<BabysitterDAO, Long> {
+}
