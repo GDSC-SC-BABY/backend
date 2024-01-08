72972719c23c554eb9c3046478a67f1dbc7ee644
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-01-08 17:40:28 +0900

    swagger setting

---
+++ src/main/java/com/example/scbaby/Config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.scbaby.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        List<Server> objects = new ArrayList<>();
+        objects.add(new Server().url("/"));
+        return new OpenAPI().servers(objects)
+                .info(new Info()
+                        .title("DevHive API")
+                        .version("1.0")
+                        .description("Base URL: /"));
+    }
+}
