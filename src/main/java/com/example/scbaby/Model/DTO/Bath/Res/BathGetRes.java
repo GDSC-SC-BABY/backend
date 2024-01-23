52ff087d1a8a65b03db557b37d075699e4fc4c7a
jjeongdong <donghun3296@naver.com>
2024-01-24 01:19:54 +0900

    feat : Bath 생활패턴 CRUD 기능 추가

---
+++ src/main/java/com/example/scbaby/Model/DTO/Bath/Res/BathGetRes.java
@@ -0,0 +1,29 @@
+package com.example.scbaby.Model.DTO.Bath.Res;
+
+import com.example.scbaby.Model.DAO.BathDAO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BathGetRes {
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private String memo;
+
+    public static BathGetRes of(BathDAO bathDAO) {
+        return BathGetRes.builder()
+                .startTime(bathDAO.getStartTime())
+                .endTime(bathDAO.getEndTime())
+                .memo(bathDAO.getMemo())
+                .build();
+    }
+}
