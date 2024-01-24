435c2452c570961fd52d8a4e1350378f0331f001
jjeongdong <donghun3296@naver.com>
2024-01-24 11:37:02 +0900

    feat : 날짜별 유아 생활패턴 조회 기능 추가

--- src/main/java/com/example/scbaby/Repository/BathRepository.java
+++ src/main/java/com/example/scbaby/Repository/BathRepository.java
@@ -1,7 +1,12 @@
 package com.example.scbaby.Repository;
 
+import com.example.scbaby.Model.DAO.BabyDAO;
 import com.example.scbaby.Model.DAO.BathDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface BathRepository extends JpaRepository<BathDAO, Long> {
+    List<BathDAO> findByBabyAndStartTimeBetweenOrderByStartTime(BabyDAO baby, LocalDateTime startTime, LocalDateTime endTime);
 }
--- src/main/java/com/example/scbaby/Repository/MedicineRepository.java
+++ src/main/java/com/example/scbaby/Repository/MedicineRepository.java
@@ -1,7 +1,13 @@
 package com.example.scbaby.Repository;
 
+import com.example.scbaby.Model.DAO.BabyDAO;
 import com.example.scbaby.Model.DAO.MedicineDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface MedicineRepository extends JpaRepository<MedicineDAO, Long> {
+    List<MedicineDAO> findByBabyAndStartTimeBetweenOrderByStartTime(BabyDAO baby, LocalDateTime startTime, LocalDateTime endTime);
+
 }
