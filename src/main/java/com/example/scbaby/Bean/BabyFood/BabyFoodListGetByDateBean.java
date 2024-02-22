4a1ff1ee405a62082b4651ea7da81dab6e344b9b
jjeongdong <donghun3296@naver.com>
2024-02-22 19:13:40 +0900

    fix : 충돌 제거

--- src/main/java/com/example/scbaby/Bean/BabyFood/BabyFoodListGetByDateBean.java
+++ src/main/java/com/example/scbaby/Bean/BabyFood/BabyFoodListGetByDateBean.java
@@ -6,10 +6,7 @@ import com.example.scbaby.Model.DTO.BabyFood.Res.BabyFoodGetRes;
 import com.example.scbaby.Model.DTO.BabyFood.Res.BabyFoodListRes;
 import com.example.scbaby.Repository.BabyFoodRepository;
 import com.example.scbaby.Repository.BabyRepository;
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
-=======
->>>>>>> a9cfa5f44a2022f4e458f5046f6a4739f1f0c6a7
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -18,24 +15,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-<<<<<<< HEAD
 @RequiredArgsConstructor
 public class BabyFoodListGetByDateBean {
 
     private final BabyFoodRepository babyFoodRepository;
     private final BabyRepository babyRepository;
 
-=======
-public class BabyFoodListGetByDateBean {
-    private final BabyFoodRepository babyFoodRepository;
-    private final BabyRepository babyRepository;
-
-    public BabyFoodListGetByDateBean(BabyFoodRepository babyFoodRepository, BabyRepository babyRepository) {
-        this.babyFoodRepository = babyFoodRepository;
-        this.babyRepository = babyRepository;
-    }
-
->>>>>>> a9cfa5f44a2022f4e458f5046f6a4739f1f0c6a7
     public BabyFoodListRes exec(Long babyId, LocalDate date) {
         BabyDAO babyDAO = babyRepository.findByBabyId(babyId);
         LocalDateTime startDateTime = date.atStartOfDay();
